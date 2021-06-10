bbbe7368f3cff5e413101c4dd8ca8dbb5d6ef830
Joan Hermida <joan@hermida.top>
2021-06-10 00:43:13 -0500

    Changes: - Add get started bootstrap.sh script. - Update README. - Add fancy icon.

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+![container](container.png)
+<div>Icons made by <a href="https://creativemarket.com/eucalyp" title="Eucalyp">Eucalyp</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div><br/>
+
 # Lokal-Cluster
 Local Production-Class Kubernetes Cluster using Vagrant and Ansible.
 
@@ -5,6 +8,10 @@ Local Production-Class Kubernetes Cluster using Vagrant and Ansible.
 This project was made following [Kubernetes Setup Using Ansible and Vagrant](https://kubernetes.io/blog/2019/03/15/kubernetes-setup-using-ansible-and-vagrant/) as reference. Please, refer to that article for any further information.
 
 # Prerequisites
-- Vagrant should be installed on your machine. Installation binaries can be found [here](https://www.vagrantup.com/downloads.html).
-- Oracle VirtualBox can be used as a Vagrant provider or make use of similar providers as described in Vagrant's official [documentation](https://www.vagrantup.com/docs/providers/).
-- Ansible should be installed in your machine. Refer to the [Ansible installation guide](https://docs.ansible.com/ansible/latest/installation_guide/intro_installation.html) for platform specific installation.
+- VirtualBox must to be present on your system. Vagrant will seek for a version of Oracle VirtualBox in your localhost to use it as a default [provider](https://www.vagrantup.com/docs/providers).
+
+# Warning
+- If you have any other cluster definition in `~/.kube/config`, please make a copy of your current `~/.kube/config`
+
+# Getting Started
+1. Run bootstrap.sh
---
+++ bootstrap.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+#set -x
+
+echo -e "\e[39mVagrant...is it present?"
+
+if ! command -v vagrant &> /dev/null
+then
+
+    echo -e "\e[31mVagrant not present...\n\e[39mInstalling..."
+    
+    version_vg=$(curl -s https://releases.hashicorp.com/vagrant/ | grep href | grep -v '\.\.' | head -1 | awk -F/ '{ print $3 }')
+    
+    curl -SLO https://releases.hashicorp.com/vagrant/${version_vg}/vagrant_${version_vg}_linux_amd64.zip
+    
+    curl -sSLO https://releases.hashicorp.com/vagrant/${version_vg}/vagrant_${version_vg}_SHA256SUMS
+    
+    checksum_vg=$(sha256sum -c vagrant_${version_vg}_SHA256SUMS 2>&1 | grep OK | awk -F:' ' '{ print $2 }')
+
+    if [ "$checksum_vg" != "OK" ]
+    then
+        exit 1
+    fi
+
+    unzip vagrant_${version_vg}_linux_amd64.zip && rm vagrant_${version_vg}_linux_amd64.zip vagrant_${version_vg}_SHA256SUMS
+    
+    sudo mv vagrant /usr/bin/vagrant
+
+    echo -e "\e[32mDone!"
+
+else
+    
+    echo -e "\e[32mVagrant is present!"
+
+fi
+
+# echo -e "\e[39mPlugins..."
+
+# vagrant plugin install virtualbox ansible
+
+echo -e "\e[39mProvider..."
+
+if ! command -v virtualbox &> /dev/null
+then
+
+    echo -e "\e[31mVirtualBox not present...\nPlease, install a stable version of Oracle VirtualBox"
+
+    exit 1
+
+else
+
+    echo -e "\e[32mVirtualBox is present!"
+
+fi
+
+echo -e "\e[39mInitializing...\nPlease, be aware this could take several minutes."
+
+vagrant up --provider=virtualbox
+
+until [ $(vagrant status | sed 1,2d | head -n3 | grep -o 'running' | wc -l) == 3 ]
+do
+    sleep 3 && echo "...waiting for status: running"
+done
+
+echo -e "\e[32mUp and Running!"
+
+echo -e "\e[39mkubectl...is it present?"
+
+if ! command -v kubectl &> /dev/null
+then
+
+    echo -e "\e[31mkubectl not present\n\e[39mDownloading..."
+    
+    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+    
+    curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+    
+    checksum_kctl=$(echo "$(<kubectl.sha256) kubectl" | sha256sum --check | awk -F:' ' '{ print $2 }')
+
+    if [ "$checksum_kctl" != "OK" ]
+    then
+        exit 1
+    fi
+
+    echo -e "\e[39mInstalling kubectl"
+    
+    sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl && mkdir ~/.kube && rm kubectl.sha256
+
+else
+
+    echo -e "\e[32mkubectl is present!"
+
+fi
+
+echo -e "\e[39mConfiguring..."
+
+vagrant ssh master -- -t 'sudo cat /etc/kubernetes/admin.conf' > ~/.kube/config
+
+echo -e "\e[32mDone!\nEnjoy your new cluster"
